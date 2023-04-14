237847b9289582cda8effbec61ef93c3ecea1804
varrbor <varrbor@ukr.net>
2023-04-14 05:29:51 -0700

    Fixed styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,36 +34,46 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <div className="inputs">
-        <div className="rooms">
-          <label htmlFor="cheese">Free Premium rooms:</label>
+    <div className="flex flex-col items-center justify-center mt-20">
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center mt-5">
+          <label htmlFor="cheese">Enter Free Premium rooms:</label>
           <input
             value={roomsNumber.premium}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             name="premium"
             type="number"
             onChange={onRoomsChanged}
           ></input>
         </div>
-        <div className="rooms">
-          <label htmlFor="cheese">Free Economy rooms:</label>
+        <div className="flex flex-col items-center justify-center mt-5">
+          <label htmlFor="cheese">Enter Free Econom rooms:</label>
           <input
             value={roomsNumber.econom}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             name="econom"
             type="number"
             onChange={onRoomsChanged}
           ></input>
         </div>
-        <button onClick={onClickHandler}>Calculate</button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"
+          onClick={onClickHandler}
+        >
+          Calculate
+        </button>
       </div>
-      <div className="roomsUsage">
-        <div>
-          Usage Premium: {calculatedData.premium.occupied} (EUR{' '}
-          {calculatedData.premium.profit})
-        </div>
-        <div>
-          Usage Economy: {calculatedData.econom.occupied} (EUR{' '}
-          {calculatedData.econom.profit})
+      <div className="mt-5">
+        <div className="flex  justify-center mt-2">Result</div>
+        <div className="flex flex-col  items-center justify-center mt-2">
+          <div>
+            Usage Premium: {calculatedData.premium.occupied} (EUR
+            {calculatedData.premium.profit})
+          </div>
+          <div className=" mt-2">
+            Usage Economy: {calculatedData.econom.occupied} (EUR
+            {calculatedData.econom.profit})
+          </div>
         </div>
       </div>
     </div>
